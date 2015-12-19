XRPLF/rippled

Improve STTx construction:
* Remove the ability to construct an empty transaction by type, only
  to then have to add fields to it. Instead, offer a constructor that
  accepts a transaction type and a lambda that can insert fields into
  the STTx during construction.
* Remove now obsolete boost::optional transaction ID.

src/ripple/app/misc/AmendmentTable.h
@@ -272,22 +272,25 @@ public:
         // Inject appropriate pseudo-transactions
         for (auto const& it : actions)
         {
-            STTx trans (ttAMENDMENT);
-            trans.setAccountID (sfAccount, AccountID());
-            trans.setFieldH256 (sfAmendment, it.first);
-            trans.setFieldU32 (sfLedgerSequence, lastClosedLedger->seq() + 1);
+            STTx amendTx (ttAMENDMENT,
+                [&it, seq = lastClosedLedger->seq() + 1](STObject& obj)
+                {
+                    obj.setAccountID (sfAccount, AccountID());
+                    obj.setFieldH256 (sfAmendment, it.first);
+                    obj.setFieldU32 (sfLedgerSequence, seq);
 
-            if (it.second != 0)
-                trans.setFieldU32 (sfFlags, it.second);
+                    if (it.second != 0)
+                        obj.setFieldU32 (sfFlags, it.second);
+                });
 
             Serializer s;
-            trans.add (s);
+            amendTx.add (s);
 
         #if ! RIPPLE_PROPOSE_AMENDMENTS
             return;
         #endif
 
-            uint256 txID = trans.getTransactionID();
+            uint256 txID = amendTx.getTransactionID();
             auto tItem = std::make_shared <SHAMapItem> (txID, s.peekData());
             initialPosition->addGiveItem (tItem, true, false);
         }

src/ripple/app/misc/FeeVoteImpl.cpp
@@ -201,6 +201,7 @@ FeeVoteImpl::doVoting (Ledger::ref lastClosedLedger,
     std::uint64_t const baseFee = baseFeeVote.getVotes ();
     std::uint32_t const baseReserve = baseReserveVote.getVotes ();
     std::uint32_t const incReserve = incReserveVote.getVotes ();
+    std::uint32_t const feeUnits = target_.reference_fee_units;
 
     // add transactions to our position
     if ((baseFee != lastClosedLedger->fees().base) ||
@@ -212,20 +213,23 @@ FeeVoteImpl::doVoting (Ledger::ref lastClosedLedger,
             "/" << baseReserve <<
             "/" << incReserve;
 
-        STTx trans (ttFEE);
-        trans[sfAccount] = AccountID();
-        trans[sfBaseFee] = baseFee;
-        trans[sfReferenceFeeUnits] = target_.reference_fee_units;
-        trans[sfReserveBase] = baseReserve;
-        trans[sfReserveIncrement] = incReserve;
+        STTx feeTx (ttFEE,
+            [baseFee,baseReserve,incReserve,feeUnits](STObject& obj)
+            {
+                obj[sfAccount] = AccountID();
+                obj[sfBaseFee] = baseFee;
+                obj[sfReserveBase] = baseReserve;
+                obj[sfReserveIncrement] = incReserve;
+                obj[sfReferenceFeeUnits] = feeUnits;
+            });
 
-        uint256 txID = trans.getTransactionID ();
+        uint256 txID = feeTx.getTransactionID ();
 
         if (journal_.warning)
             journal_.warning << "Vote: " << txID;
 
         Serializer s;
-        trans.add (s);
+        feeTx.add (s);
 
         auto tItem = std::make_shared<SHAMapItem> (txID, s.peekData ());
 

src/ripple/protocol/tests/STTx.test.cpp
@@ -38,10 +38,13 @@ public:
         RippleAddress publicAcct = RippleAddress::createAccountPublic (generator, 1);
         RippleAddress privateAcct = RippleAddress::createAccountPrivate (generator, seed, 1);
 
-        STTx j (ttACCOUNT_SET);
-        j.setAccountID (sfAccount, calcAccountID(publicAcct));
-        j.setSigningPubKey (publicAcct);
-        j.setFieldVL (sfMessageKey, publicAcct.getAccountPublic ());
+        STTx j (ttACCOUNT_SET,
+            [&publicAcct](STObject& obj)
+            {
+                obj.setAccountID (sfAccount, calcAccountID(publicAcct));
+                obj.setFieldVL (sfMessageKey, publicAcct.getAccountPublic ());
+                obj.setFieldVL (sfSigningPubKey, publicAcct.getAccountPublic ());
+            });
         j.sign (privateAcct);
 
         unexpected (!j.checkSign (true), "Transaction fails signature test");
@@ -92,12 +95,14 @@ public:
         // VFALCO Use PublicKey here
         RippleAddress txnPublicAcct = txnSeed.createAccountPublic (txnGenerator, 1);
 
-        STTx txn (ttACCOUNT_SET);
-        txn.setAccountID (sfAccount, calcAccountID(txnPublicAcct));
-        txn.setSigningPubKey (txnPublicAcct);
-        txn.setFieldVL (sfMessageKey, txnPublicAcct.getAccountPublic ());
-        Blob const emptyBlob;  // Make empty signature for multi-signing
-        txn.setFieldVL (sfSigningPubKey, emptyBlob);
+        STTx txn (ttACCOUNT_SET,
+            [&txnPublicAcct](STObject& obj)
+            {
+                obj.setAccountID (sfAccount, calcAccountID(txnPublicAcct));
+                obj.setFieldVL (sfMessageKey, txnPublicAcct.getAccountPublic ());
+                // Make empty signature for multi-signing
+                obj.setFieldVL (sfSigningPubKey, {});
+            });
 
         // Create fields for a SigningAccount
         RippleAddress saSeed;

beartec-jpg/FalconLedger

src/ripple/app/misc/AmendmentTable.h
@@ -272,22 +272,25 @@ public:
         // Inject appropriate pseudo-transactions
         for (auto const& it : actions)
         {
-            STTx trans (ttAMENDMENT);
-            trans.setAccountID (sfAccount, AccountID());
-            trans.setFieldH256 (sfAmendment, it.first);
-            trans.setFieldU32 (sfLedgerSequence, lastClosedLedger->seq() + 1);
+            STTx amendTx (ttAMENDMENT,
+                [&it, seq = lastClosedLedger->seq() + 1](auto& obj)
+                {
+                    obj.setAccountID (sfAccount, AccountID());
+                    obj.setFieldH256 (sfAmendment, it.first);
+                    obj.setFieldU32 (sfLedgerSequence, seq);
 
-            if (it.second != 0)
-                trans.setFieldU32 (sfFlags, it.second);
+                    if (it.second != 0)
+                        obj.setFieldU32 (sfFlags, it.second);
+                });
 
             Serializer s;
-            trans.add (s);
+            amendTx.add (s);
 
         #if ! RIPPLE_PROPOSE_AMENDMENTS
             return;
         #endif
 
-            uint256 txID = trans.getTransactionID();
+            uint256 txID = amendTx.getTransactionID();
             auto tItem = std::make_shared <SHAMapItem> (txID, s.peekData());
             initialPosition->addGiveItem (tItem, true, false);
         }

src/ripple/app/misc/FeeVoteImpl.cpp
@@ -201,6 +201,7 @@ FeeVoteImpl::doVoting (Ledger::ref lastClosedLedger,
     std::uint64_t const baseFee = baseFeeVote.getVotes ();
     std::uint32_t const baseReserve = baseReserveVote.getVotes ();
     std::uint32_t const incReserve = incReserveVote.getVotes ();
+    std::uint32_t const feeUnits = target_.reference_fee_units;
 
     // add transactions to our position
     if ((baseFee != lastClosedLedger->fees().base) ||
@@ -212,20 +213,23 @@ FeeVoteImpl::doVoting (Ledger::ref lastClosedLedger,
             "/" << baseReserve <<
             "/" << incReserve;
 
-        STTx trans (ttFEE);
-        trans[sfAccount] = AccountID();
-        trans[sfBaseFee] = baseFee;
-        trans[sfReferenceFeeUnits] = target_.reference_fee_units;
-        trans[sfReserveBase] = baseReserve;
-        trans[sfReserveIncrement] = incReserve;
+        STTx feeTx (ttFEE,
+            [baseFee,baseReserve,incReserve,feeUnits](auto& obj)
+            {
+                obj[sfAccount] = AccountID();
+                obj[sfBaseFee] = baseFee;
+                obj[sfReserveBase] = baseReserve;
+                obj[sfReserveIncrement] = incReserve;
+                obj[sfReferenceFeeUnits] = feeUnits;
+            });
 
-        uint256 txID = trans.getTransactionID ();
+        uint256 txID = feeTx.getTransactionID ();
 
         if (journal_.warning)
             journal_.warning << "Vote: " << txID;
 
         Serializer s;
-        trans.add (s);
+        feeTx.add (s);
 
         auto tItem = std::make_shared<SHAMapItem> (txID, s.peekData ());
 

src/ripple/protocol/tests/STTx.test.cpp
@@ -38,10 +38,13 @@ public:
         RippleAddress publicAcct = RippleAddress::createAccountPublic (generator, 1);
         RippleAddress privateAcct = RippleAddress::createAccountPrivate (generator, seed, 1);
 
-        STTx j (ttACCOUNT_SET);
-        j.setAccountID (sfAccount, calcAccountID(publicAcct));
-        j.setSigningPubKey (publicAcct);
-        j.setFieldVL (sfMessageKey, publicAcct.getAccountPublic ());
+        STTx j (ttACCOUNT_SET,
+            [&publicAcct](auto& obj)
+            {
+                obj.setAccountID (sfAccount, calcAccountID(publicAcct));
+                obj.setFieldVL (sfMessageKey, publicAcct.getAccountPublic ());
+                obj.setFieldVL (sfSigningPubKey, publicAcct.getAccountPublic ());
+            });
         j.sign (privateAcct);
 
         unexpected (!j.checkSign (true), "Transaction fails signature test");
@@ -92,12 +95,14 @@ public:
         // VFALCO Use PublicKey here
         RippleAddress txnPublicAcct = txnSeed.createAccountPublic (txnGenerator, 1);
 
-        STTx txn (ttACCOUNT_SET);
-        txn.setAccountID (sfAccount, calcAccountID(txnPublicAcct));
-        txn.setSigningPubKey (txnPublicAcct);
-        txn.setFieldVL (sfMessageKey, txnPublicAcct.getAccountPublic ());
-        Blob const emptyBlob;  // Make empty signature for multi-signing
-        txn.setFieldVL (sfSigningPubKey, emptyBlob);
+        STTx txn (ttACCOUNT_SET,
+            [&txnPublicAcct](auto& obj)
+            {
+                obj.setAccountID (sfAccount, calcAccountID(txnPublicAcct));
+                obj.setFieldVL (sfMessageKey, txnPublicAcct.getAccountPublic ());
+                // Make empty signature for multi-signing
+                obj.setFieldVL (sfSigningPubKey, {});
+            });
 
         // Create fields for a SigningAccount
         RippleAddress saSeed;