XRPLF/rippled

Convert PathRequest to use std::chrono (RIPD-1069)

src/ripple/app/paths/PathRequest.h
@@ -144,9 +144,9 @@ private:
 
     int iIdentifier;
 
-    boost::posix_time::ptime ptCreated;
-    boost::posix_time::ptime ptQuickReply;
-    boost::posix_time::ptime ptFullReply;
+    std::chrono::high_resolution_clock::time_point const created_;
+    std::chrono::high_resolution_clock::time_point quick_reply_;
+    std::chrono::high_resolution_clock::time_point full_reply_;
 
     static unsigned int const max_paths_ = 4;
 };

beartec-jpg/FalconLedger

src/ripple/app/paths/PathRequest.h
@@ -144,9 +144,9 @@ private:
 
     int iIdentifier;
 
-    boost::posix_time::ptime ptCreated;
-    boost::posix_time::ptime ptQuickReply;
-    boost::posix_time::ptime ptFullReply;
+    std::chrono::steady_clock::time_point const created_;
+    std::chrono::steady_clock::time_point quick_reply_;
+    std::chrono::steady_clock::time_point full_reply_;
 
     static unsigned int const max_paths_ = 4;
 };