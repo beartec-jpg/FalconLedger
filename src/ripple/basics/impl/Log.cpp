XRPLF/rippled

Simplify logging: * Construct Logs with the correct severity * Remove deprecatedLogs and log squelching support * Use debugJournal for AutoSocket logging

src/ripple/basics/impl/Log.cpp
@@ -108,8 +108,8 @@ void Logs::File::writeln (char const* text)
 
 //------------------------------------------------------------------------------
 
-Logs::Logs()
-    : level_ (beast::Journal::kWarning) // default severity
+Logs::Logs(beast::Journal::Severity level)
+    : level_ (level) // default severity
 {
 }
 

beartec-jpg/FalconLedger

src/ripple/basics/impl/Log.cpp
@@ -111,8 +111,8 @@ void Logs::File::writeln (char const* text)
 
 //------------------------------------------------------------------------------
 
-Logs::Logs()
-    : thresh_ (beast::Journal::kWarning) // default severity
+Logs::Logs(beast::Journal::Severity thresh)
+    : thresh_ (thresh) // default severity
 {
 }
 