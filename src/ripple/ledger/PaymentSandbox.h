XRPLF/rippled

Better numerical stability for deferred credits:
Before this change, the deferred credits algorithm took the current
balance and subtracted the recorded credits. Conceptually, this is the
same as taking the original balance, adding all the credits,
subtracting all the debits, and subtracting all the credits. The new
algorithm records the original balance and subtracts the debits. This
prevents errors that occur when the original balance and the recorded
credits have large differences in magnitude.

Additionally, XRP credits were recorded incorrectly in the deferred
credits table (the line was between the sender and receiver, rather than
the root account).

src/ripple/ledger/PaymentSandbox.h
@@ -36,16 +36,26 @@ namespace detail {
 class DeferredCredits
 {
 public:
-    // Get the adjusted balance of main for the
-    // balance between main and other.
-    STAmount
-    adjustedBalance (AccountID const& main,
+    struct adjustment
+    {
+        adjustment (STAmount const& d, STAmount const& c, STAmount const& b)
+            : debits (d), credits (c), orgBalance (b) {}
+        STAmount debits;
+        STAmount credits;
+        STAmount orgBalance;
+    };
+
+    // Get the adjustments for the balance between main and other.
+    // Returns the debits, credits and the original balance
+    boost::optional<adjustment> adjustments (
+        AccountID const& main,
         AccountID const& other,
-            STAmount const& curBalance) const;
+        Currency const& currency) const;
 
     void credit (AccountID const& sender,
-                 AccountID const& receiver,
-                 STAmount const& amount);
+        AccountID const& receiver,
+        STAmount const& amount,
+        STAmount const& preCreditBalance);
 
     void apply (DeferredCredits& to);
 
@@ -57,10 +67,12 @@ private:
     // lowAccount, highAccount
     using Key = std::tuple<
         AccountID, AccountID, Currency>;
-
-    // lowAccountCredits, highAccountCredits
-    using Value = std::tuple<
-        STAmount, STAmount>;
+    struct Value
+    {
+        STAmount lowAccCredits;
+        STAmount highAccCredits;
+        STAmount lowAccOrgBalance;
+    };
 
     static
     Key
@@ -145,7 +157,8 @@ public:
     void
     creditHook (AccountID const& from,
         AccountID const& to,
-            STAmount const& amount) override;
+            STAmount const& amount,
+                STAmount const& preCreditBalance) override;
 
     /** Apply changes to base view.
 
@@ -154,8 +167,7 @@ public:
         behavior will result.
     */
     /** @{ */
-    void
-    apply (RawView& to);
+    void apply (RawView& to);
 
     void
     apply (PaymentSandbox& to);

beartec-jpg/FalconLedger

src/ripple/ledger/PaymentSandbox.h
@@ -36,31 +36,38 @@ namespace detail {
 class DeferredCredits
 {
 public:
-    // Get the adjusted balance of main for the
-    // balance between main and other.
-    STAmount
-    adjustedBalance (AccountID const& main,
+    struct Adjustment
+    {
+        Adjustment (STAmount const& d, STAmount const& c, STAmount const& b)
+            : debits (d), credits (c), origBalance (b) {}
+        STAmount debits;
+        STAmount credits;
+        STAmount origBalance;
+    };
+
+    // Get the adjustments for the balance between main and other.
+    // Returns the debits, credits and the original balance
+    boost::optional<Adjustment> adjustments (
+        AccountID const& main,
         AccountID const& other,
-            STAmount const& curBalance) const;
+        Currency const& currency) const;
 
     void credit (AccountID const& sender,
-                 AccountID const& receiver,
-                 STAmount const& amount);
+        AccountID const& receiver,
+        STAmount const& amount,
+        STAmount const& preCreditSenderBalance);
 
     void apply (DeferredCredits& to);
-
-    // VFALCO Is this needed?
-    // DEPRECATED
-    void clear ();
-
 private:
     // lowAccount, highAccount
     using Key = std::tuple<
         AccountID, AccountID, Currency>;
-
-    // lowAccountCredits, highAccountCredits
-    using Value = std::tuple<
-        STAmount, STAmount>;
+    struct Value
+    {
+        STAmount lowAcctCredits;
+        STAmount highAcctCredits;
+        STAmount lowAcctOrigBalance;
+    };
 
     static
     Key
@@ -145,7 +152,8 @@ public:
     void
     creditHook (AccountID const& from,
         AccountID const& to,
-            STAmount const& amount) override;
+            STAmount const& amount,
+                STAmount const& preCreditBalance) override;
 
     /** Apply changes to base view.
 
@@ -154,8 +162,7 @@ public:
         behavior will result.
     */
     /** @{ */
-    void
-    apply (RawView& to);
+    void apply (RawView& to);
 
     void
     apply (PaymentSandbox& to);