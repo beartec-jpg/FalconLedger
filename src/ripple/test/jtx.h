XRPLF/rippled

JTx sign-and-submit mode support

src/ripple/test/jtx.h
@@ -47,6 +47,7 @@
 #include <ripple/test/jtx/sendmax.h>
 #include <ripple/test/jtx/seq.h>
 #include <ripple/test/jtx/sig.h>
+#include <ripple/test/jtx/signandsubmit.h>
 #include <ripple/test/jtx/tag.h>
 #include <ripple/test/jtx/tags.h>
 #include <ripple/test/jtx/ter.h>

beartec-jpg/FalconLedger

src/ripple/test/jtx.h
@@ -28,6 +28,7 @@
 #include <ripple/test/jtx/balance.h>
 #include <ripple/test/jtx/delivermin.h>
 #include <ripple/test/jtx/Env.h>
+#include <ripple/test/jtx/Env_ss.h>
 #include <ripple/test/jtx/fee.h>
 #include <ripple/test/jtx/flags.h>
 #include <ripple/test/jtx/jtx_json.h>