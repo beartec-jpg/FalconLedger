XRPLF/rippled

Cryptoconditions: ED25519 (RIPD-1214)

src/ripple/unity/conditions.cpp
@@ -23,3 +23,4 @@
 #include <ripple/conditions/impl/fulfillment.cpp>
 
 #include <ripple/conditions/tests/hashlock_test.cpp>
+#include <ripple/conditions/tests/ed25519_test.cpp>

beartec-jpg/FalconLedger

src/ripple/unity/conditions.cpp
@@ -21,3 +21,4 @@
 
 #include <ripple/conditions/impl/Condition.cpp>
 #include <ripple/conditions/impl/Fulfillment.cpp>
+#include <ripple/conditions/impl/Ed25519.cpp>