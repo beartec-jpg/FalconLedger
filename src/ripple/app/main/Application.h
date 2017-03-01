wilsonianb/rippled

Dynamize trusted validator list and quorum (RIPD-1220):
Instead of specifying a static list of trusted validators in the config
or validators file, the configuration can now include trusted validator
list publisher keys.
Publisher public keys should use the account public Base58 encoding to
distinguish them from node public keys.

The trusted validator list and quorum are now reset each consensus
round using the latest validator lists and the list of recent
validations seen. The minimum validation quorum is now only
configurable via the command line.

src/ripple/app/main/Application.h
@@ -50,6 +50,7 @@ class InboundTransactions;
 class AcceptedLedger;
 class LedgerMaster;
 class LoadManager;
+class ManifestCache;
 class NetworkOPs;
 class OpenLedger;
 class OrderBookDB;
@@ -120,6 +121,7 @@ public:
     virtual Overlay&                overlay () = 0;
     virtual TxQ&                    getTxQ() = 0;
     virtual ValidatorList&          validators () = 0;
+    virtual ManifestCache&          manifestCache () = 0;
     virtual Cluster&                cluster () = 0;
     virtual Validations&            getValidations () = 0;
     virtual NodeStore::Database&    getNodeStore () = 0;

beartec-jpg/FalconLedger

src/ripple/app/main/Application.h
@@ -50,6 +50,7 @@ class InboundTransactions;
 class AcceptedLedger;
 class LedgerMaster;
 class LoadManager;
+class ManifestCache;
 class NetworkOPs;
 class OpenLedger;
 class OrderBookDB;
@@ -120,6 +121,8 @@ public:
     virtual Overlay&                overlay () = 0;
     virtual TxQ&                    getTxQ() = 0;
     virtual ValidatorList&          validators () = 0;
+    virtual ManifestCache&          validatorManifests () = 0;
+    virtual ManifestCache&          publisherManifests () = 0;
     virtual Cluster&                cluster () = 0;
     virtual Validations&            getValidations () = 0;
     virtual NodeStore::Database&    getNodeStore () = 0;