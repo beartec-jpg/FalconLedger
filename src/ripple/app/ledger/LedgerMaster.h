XRPLF/rippled

Replace UptimeTimer with UptimeClock
* UptimeClock is a chrono-compatible seconds-precision clock.

* Like UptimeTimer, its purpose is to make it possible for clients
  to query the uptime thousands of times per second without a
  significant performance hit.

* UptimeClock decouples itself from LoadManager by managing its
  own once-per-second update loop.

* Clients now traffic in chrono time_points and durations instead
  of int.

src/ripple/app/ledger/LedgerMaster.h
@@ -243,7 +243,7 @@ public:
         std::weak_ptr<Peer> const& wPeer,
         std::shared_ptr<protocol::TMGetObjectByHash> const& request,
         uint256 haveLedgerHash,
-        std::uint32_t uUptime);
+        UptimeClock::time_point uUptime);
 
     std::size_t getFetchPackCacheSize () const;
 

src/ripple/core/LoadMonitor.h
@@ -20,6 +20,7 @@
 #ifndef RIPPLE_CORE_LOADMONITOR_H_INCLUDED
 #define RIPPLE_CORE_LOADMONITOR_H_INCLUDED
 
+#include <ripple/basics/UptimeTimer.h>
 #include <ripple/core/LoadEvent.h>
 #include <ripple/beast/utility/Journal.h>
 #include <chrono>
@@ -73,7 +74,7 @@ private:
     std::chrono::milliseconds mLatencyMSPeak;
     std::chrono::milliseconds mTargetLatencyAvg;
     std::chrono::milliseconds mTargetLatencyPk;
-    int                       mLastUpdate;
+    UptimeClock::time_point   mLastUpdate;
     beast::Journal j_;
 };
 

beartec-jpg/FalconLedger

src/ripple/app/ledger/LedgerMaster.h
@@ -238,7 +238,7 @@ public:
         std::weak_ptr<Peer> const& wPeer,
         std::shared_ptr<protocol::TMGetObjectByHash> const& request,
         uint256 haveLedgerHash,
-        std::uint32_t uUptime);
+        UptimeClock::time_point uptime);
 
     std::size_t getFetchPackCacheSize () const;
 

src/ripple/core/LoadMonitor.h
@@ -20,6 +20,7 @@
 #ifndef RIPPLE_CORE_LOADMONITOR_H_INCLUDED
 #define RIPPLE_CORE_LOADMONITOR_H_INCLUDED
 
+#include <ripple/basics/UptimeClock.h>
 #include <ripple/core/LoadEvent.h>
 #include <ripple/beast/utility/Journal.h>
 #include <chrono>
@@ -73,7 +74,7 @@ private:
     std::chrono::milliseconds mLatencyMSPeak;
     std::chrono::milliseconds mTargetLatencyAvg;
     std::chrono::milliseconds mTargetLatencyPk;
-    int                       mLastUpdate;
+    UptimeClock::time_point   mLastUpdate;
     beast::Journal j_;
 };
 