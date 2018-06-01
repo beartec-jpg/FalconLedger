XRPLF/rippled

Convert macros in STTX.h into an enum

src/ripple/app/misc/impl/Transaction.cpp
@@ -63,18 +63,18 @@ void Transaction::setStatus (TransStatus ts, std::uint32_t lseq)
 TransStatus Transaction::sqlTransactionStatus(
     boost::optional<std::string> const& status)
 {
-    char const c = (status) ? (*status)[0] : TXN_SQL_UNKNOWN;
+    char const c = (status) ? (*status)[0] : txnSQL_UNKNOWN;
 
     switch (c)
     {
-    case TXN_SQL_NEW:       return NEW;
-    case TXN_SQL_CONFLICT:  return CONFLICTED;
-    case TXN_SQL_HELD:      return HELD;
-    case TXN_SQL_VALIDATED: return COMMITTED;
-    case TXN_SQL_INCLUDED:  return INCLUDED;
+    case txnSQL_NEW:       return NEW;
+    case txnSQL_CONFLICT:  return CONFLICTED;
+    case txnSQL_HELD:      return HELD;
+    case txnSQL_VALIDATED: return COMMITTED;
+    case txnSQL_INCLUDED:  return INCLUDED;
     }
 
-    assert (c == TXN_SQL_UNKNOWN);
+    assert (c == txnSQL_UNKNOWN);
     return INVALID;
 }
 

src/ripple/protocol/impl/STTx.cpp
@@ -240,7 +240,7 @@ std::string STTx::getMetaSQL (std::uint32_t inLedger,
 {
     Serializer s;
     add (s);
-    return getMetaSQL (s, inLedger, TXN_SQL_VALIDATED, escapedMetaData);
+    return getMetaSQL (s, inLedger, txnSQL_VALIDATED, escapedMetaData);
 }
 
 // VFALCO This could be a free function elsewhere

beartec-jpg/FalconLedger

src/ripple/app/misc/impl/Transaction.cpp
@@ -63,18 +63,18 @@ void Transaction::setStatus (TransStatus ts, std::uint32_t lseq)
 TransStatus Transaction::sqlTransactionStatus(
     boost::optional<std::string> const& status)
 {
-    char const c = (status) ? (*status)[0] : TXN_SQL_UNKNOWN;
+    char const c = (status) ? (*status)[0] : txnSqlUnknown;
 
     switch (c)
     {
-    case TXN_SQL_NEW:       return NEW;
-    case TXN_SQL_CONFLICT:  return CONFLICTED;
-    case TXN_SQL_HELD:      return HELD;
-    case TXN_SQL_VALIDATED: return COMMITTED;
-    case TXN_SQL_INCLUDED:  return INCLUDED;
+    case txnSqlNew:       return NEW;
+    case txnSqlConflict:  return CONFLICTED;
+    case txnSqlHeld:      return HELD;
+    case txnSqlValidated: return COMMITTED;
+    case txnSqlIncluded:  return INCLUDED;
     }
 
-    assert (c == TXN_SQL_UNKNOWN);
+    assert (c == txnSqlUnknown);
     return INVALID;
 }
 

src/ripple/protocol/impl/STTx.cpp
@@ -240,7 +240,7 @@ std::string STTx::getMetaSQL (std::uint32_t inLedger,
 {
     Serializer s;
     add (s);
-    return getMetaSQL (s, inLedger, TXN_SQL_VALIDATED, escapedMetaData);
+    return getMetaSQL (s, inLedger, txnSqlValidated, escapedMetaData);
 }
 
 // VFALCO This could be a free function elsewhere