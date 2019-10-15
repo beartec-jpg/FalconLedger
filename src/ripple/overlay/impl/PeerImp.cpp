XRPLF/rippled

Add metrics for PeerImp to track bandwidth usage

src/ripple/overlay/impl/PeerImp.cpp
@@ -375,6 +375,12 @@ PeerImp::json()
         }
     }
 
+    ret[jss::metrics] = Json::Value(Json::objectValue);
+    ret[jss::metrics][jss::bytes_recv_total] = Json::UInt(metrics_.recv.total_bytes());
+    ret[jss::metrics][jss::bytes_sent_total] = Json::UInt(metrics_.sent.total_bytes());
+    ret[jss::metrics][jss::bytes_recv_avg] = Json::UInt(metrics_.recv.average_bytes());
+    ret[jss::metrics][jss::bytes_sent_avg] = Json::UInt(metrics_.sent.average_bytes());
+
     return ret;
 }
 
@@ -846,6 +852,8 @@ PeerImp::onReadMessage (error_code ec, std::size_t bytes_transferred)
             stream << "onReadMessage";
     }
 
+    metrics_.recv.add_message(bytes_transferred);
+
     read_buffer_.commit (bytes_transferred);
 
     while (read_buffer_.size() > 0)
@@ -892,6 +900,8 @@ PeerImp::onWriteMessage (error_code ec, std::size_t bytes_transferred)
         else
             stream << "onWriteMessage";
     }
+
+    metrics_.sent.add_message(bytes_transferred);
 
     assert(! send_queue_.empty());
     send_queue_.pop();
@@ -2850,5 +2860,37 @@ PeerImp::isHighLatency() const
     std::lock_guard sl (recentLock_);
     return latency_ >= Tuning::peerHighLatency;
 }
+
+void
+PeerImp::Metrics::add_message(std::uint64_t bytes)
+{
+    using namespace std::chrono_literals;
+
+    totalBytes += bytes;
+    accumBytes += bytes;
+    auto const actualInterval = clock_type::now() - intervalStart;
+
+    if (actualInterval >= 1s)
+    {
+        auto const avgBytes = accumBytes / std::chrono::duration_cast<std::chrono::seconds>(actualInterval).count();
+        rollingAvg[currIndex] = avgBytes;
+        rollingAvgBytes = std::accumulate(rollingAvg.begin(), rollingAvg.end(), 0ull) / rollingAvg.size();
+
+        currIndex = (currIndex + 1) % rollingAvg.size();
+        intervalStart = clock_type::now();
+        accumBytes = 0;
+    }
+}
+
+std::uint64_t
+PeerImp::Metrics::average_bytes() const {
+    return rollingAvgBytes;
+}
+
+std::uint64_t
+PeerImp::Metrics::total_bytes() const {
+    return totalBytes;
+}
+
 
 } // ripple

src/ripple/protocol/jss.h
@@ -139,6 +139,10 @@ JSS ( both );                       // in: Subscribe, Unsubscribe
 JSS ( both_sides );                 // in: Subscribe, Unsubscribe
 JSS ( build_path );                 // in: TransactionSign
 JSS ( build_version );              // out: NetworkOPs
+JSS ( bytes_recv_avg );             // out: Peers
+JSS ( bytes_sent_avg );             // out: Peers
+JSS ( bytes_recv_total );           // out: Peers
+JSS ( bytes_sent_total );           // out: Peers
 JSS ( cancel_after );               // out: AccountChannels
 JSS ( can_delete );                 // out: CanDelete
 JSS ( channel_id );                 // out: AccountChannels
@@ -338,6 +342,7 @@ JSS ( metaData );
 JSS ( metadata );                   // out: TransactionEntry
 JSS ( method );                     // RPC
 JSS ( methods );
+JSS ( metrics );                    // out: Peers
 JSS ( min_count );                  // in: GetCounts
 JSS ( min_ledger );                 // in: LedgerCleaner
 JSS ( minimum_fee );                // out: TxQ

beartec-jpg/FalconLedger

src/ripple/overlay/impl/PeerImp.cpp
@@ -45,6 +45,7 @@
 #include <algorithm>
 #include <memory>
 #include <sstream>
+#include <numeric>
 
 using namespace std::chrono_literals;
 
@@ -374,6 +375,12 @@ PeerImp::json()
                 "Unknown status: " << last_status.newstatus ();
         }
     }
+
+    ret[jss::metrics] = Json::Value(Json::objectValue);
+    ret[jss::metrics][jss::total_bytes_recv] = std::to_string(metrics_.recv.total_bytes());
+    ret[jss::metrics][jss::total_bytes_sent] = std::to_string(metrics_.sent.total_bytes());
+    ret[jss::metrics][jss::avg_bps_recv] = std::to_string(metrics_.recv.average_bytes());
+    ret[jss::metrics][jss::avg_bps_sent] = std::to_string(metrics_.sent.average_bytes());
 
     return ret;
 }
@@ -846,6 +853,8 @@ PeerImp::onReadMessage (error_code ec, std::size_t bytes_transferred)
             stream << "onReadMessage";
     }
 
+    metrics_.recv.add_message(bytes_transferred);
+
     read_buffer_.commit (bytes_transferred);
 
     while (read_buffer_.size() > 0)
@@ -892,6 +901,8 @@ PeerImp::onWriteMessage (error_code ec, std::size_t bytes_transferred)
         else
             stream << "onWriteMessage";
     }
+
+    metrics_.sent.add_message(bytes_transferred);
 
     assert(! send_queue_.empty());
     send_queue_.pop();
@@ -2850,5 +2861,42 @@ PeerImp::isHighLatency() const
     std::lock_guard sl (recentLock_);
     return latency_ >= Tuning::peerHighLatency;
 }
+
+void
+PeerImp::Metrics::add_message(std::uint64_t bytes)
+{
+    using namespace std::chrono_literals;
+    std::unique_lock lock{ mutex_ };
+
+    totalBytes_ += bytes;
+    accumBytes_ += bytes;
+    auto const timeElapsed = clock_type::now() - intervalStart_;
+    auto const timeElapsedInSecs = std::chrono::duration_cast<std::chrono::seconds>(timeElapsed);
+
+    if (timeElapsedInSecs >= 1s)
+    {
+        auto const avgBytes = accumBytes_ / timeElapsedInSecs.count();
+        rollingAvg_.push_back(avgBytes);
+
+        auto const totalBytes = std::accumulate(rollingAvg_.begin(), rollingAvg_.end(), 0ull);
+        rollingAvgBytes_ = totalBytes / rollingAvg_.size();
+
+        intervalStart_ = clock_type::now();
+        accumBytes_ = 0;
+    }
+}
+
+std::uint64_t
+PeerImp::Metrics::average_bytes() const {
+    std::shared_lock lock{ mutex_ };
+    return rollingAvgBytes_;
+}
+
+std::uint64_t
+PeerImp::Metrics::total_bytes() const {
+    std::shared_lock lock{ mutex_ };
+    return totalBytes_;
+}
+
 
 } // ripple

src/ripple/protocol/jss.h
@@ -126,6 +126,8 @@ JSS ( authorized );                 // out: AccountLines
 JSS ( auth_change );                // out: AccountInfo
 JSS ( auth_change_queued );         // out: AccountInfo
 JSS ( available );                  // out: ValidatorList
+JSS ( avg_bps_recv );               // out: Peers
+JSS ( avg_bps_sent );               // out: Peers
 JSS ( balance );                    // out: AccountLines
 JSS ( balances );                   // out: GatewayBalances
 JSS ( base );                       // out: LogLevel
@@ -338,6 +340,7 @@ JSS ( metaData );
 JSS ( metadata );                   // out: TransactionEntry
 JSS ( method );                     // RPC
 JSS ( methods );
+JSS ( metrics );                    // out: Peers
 JSS ( min_count );                  // in: GetCounts
 JSS ( min_ledger );                 // in: LedgerCleaner
 JSS ( minimum_fee );                // out: TxQ
@@ -489,6 +492,8 @@ JSS ( timeouts );                   // out: InboundLedger
 JSS ( traffic );                    // out: Overlay
 JSS ( total );                      // out: counters
 JSS ( totalCoins );                 // out: LedgerToJson
+JSS ( total_bytes_recv );           // out: Peers
+JSS ( total_bytes_sent );           // out: Peers
 JSS ( total_coins );                // out: LedgerToJson
 JSS ( transTreeHash );              // out: ledger/Ledger.cpp
 JSS ( transaction );                // in: Tx