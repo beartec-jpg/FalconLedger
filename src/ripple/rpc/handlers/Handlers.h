XRPLF/rippled

Add new validator_info and manifest rpc methods
Returns local validator details and specified manifest information
respectively. Folded and rebased on latest develop

src/ripple/rpc/handlers/Handlers.h
@@ -57,6 +57,7 @@ Json::Value doLedgerHeader          (RPC::JsonContext&);
 Json::Value doLedgerRequest         (RPC::JsonContext&);
 Json::Value doLogLevel              (RPC::JsonContext&);
 Json::Value doLogRotate             (RPC::JsonContext&);
+Json::Value doManifest              (RPC::Context&);
 Json::Value doNoRippleCheck         (RPC::JsonContext&);
 Json::Value doOwnerInfo             (RPC::JsonContext&);
 Json::Value doPathFind              (RPC::JsonContext&);
@@ -86,6 +87,7 @@ Json::Value doValidationCreate      (RPC::JsonContext&);
 Json::Value doWalletPropose         (RPC::JsonContext&);
 Json::Value doValidators            (RPC::JsonContext&);
 Json::Value doValidatorListSites    (RPC::JsonContext&);
+Json::Value doValidatorInfo         (RPC::Context&);
 } // ripple
 
 #endif

src/ripple/rpc/handlers/Manifest.cpp
@@ -1,0 +1,75 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2019 Dev Null Productions
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#include <ripple/app/main/Application.h>
+#include <ripple/basics/base64.h>
+#include <ripple/json/json_value.h>
+#include <ripple/protocol/ErrorCodes.h>
+#include <ripple/protocol/jss.h>
+#include <ripple/rpc/Context.h>
+
+namespace ripple {
+Json::Value doManifest (RPC::Context& context)
+{
+    auto& params = context.params;
+
+    if (!params.isMember(jss::public_key))
+        return RPC::missing_field_error (jss::public_key);
+
+    auto const requested = params[jss::public_key].asString();
+
+    Json::Value ret;
+    ret[jss::requested] = requested;
+
+    auto const pk = parseBase58<PublicKey>(TokenType::NodePublic, requested);
+    if (!pk)
+    {
+        RPC::inject_error(rpcINVALID_PARAMS, ret);
+        return ret;
+    }
+
+    // first attempt to use params as ephemeral key,
+    // if this lookup succeeds master key will be returned,
+    // else pk will just be returned and we will assume that
+    // is master key anyways
+    auto const mk = context.app.validatorManifests().getMasterKey(*pk);
+
+    auto const ek = context.app.validatorManifests().getSigningKey(mk);
+
+    // if ephemeral key not found, we don't have specified manifest
+    if (ek == mk)
+        return ret;
+
+    if (auto const manifest = context.app.validatorManifests().getManifest(mk))
+        ret[jss::manifest] = base64_encode(*manifest);
+    Json::Value details;
+
+    details[jss::master_key] = toBase58(TokenType::NodePublic, mk);
+    details[jss::ephemeral_key] = toBase58(TokenType::NodePublic, ek);
+
+    if (auto const seq = context.app.validatorManifests().getSequence(mk))
+        details[jss::seq] = *seq;
+
+    if (auto const domain = context.app.validatorManifests().getDomain(mk))
+        details[jss::domain] = *domain;
+
+    ret[jss::details] = details;
+    return ret;
+}
+} // ripple

src/ripple/rpc/handlers/ValidatorInfo.cpp
@@ -1,0 +1,62 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2019 Dev Null Productions
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#include <ripple/app/misc/ValidatorKeys.h>
+#include <ripple/app/main/Application.h>
+#include <ripple/basics/base64.h>
+#include <ripple/json/json_value.h>
+#include <ripple/protocol/ErrorCodes.h>
+#include <ripple/protocol/jss.h>
+#include <ripple/rpc/Context.h>
+
+namespace ripple {
+Json::Value doValidatorInfo (RPC::Context& context)
+{
+    // return error if not configured as validator
+    if (context.app.getValidationPublicKey().empty())
+      return RPC::not_validator_error ();
+
+    Json::Value ret;
+
+    auto const pk = context.app.getValidationPublicKey();
+
+    // assume pk is ephemeral key, get master key
+    auto const mk = context.app.validatorManifests().getMasterKey(pk);
+    ret[jss::master_key] = toBase58(TokenType::NodePublic, mk);
+
+    // pk is maskter key, eg no ephemeral key, eg no manifest, just return
+    if (mk == pk)
+        return ret;
+
+    // lookup ephemeral key
+    auto const ek = context.app.validatorManifests().getSigningKey(mk);
+    ret[jss::ephemeral_key] = toBase58(TokenType::NodePublic, ek);
+
+    if (auto const manifest = context.app.validatorManifests().getManifest(mk))
+        ret[jss::manifest] = base64_encode(*manifest);
+
+    if (auto const seq = context.app.validatorManifests().getSequence(mk))
+        ret[jss::seq] = *seq;
+
+    if (auto const domain = context.app.validatorManifests().getDomain(mk))
+        ret[jss::domain] = *domain;
+
+    return ret;
+}
+} // ripple

beartec-jpg/FalconLedger

src/ripple/rpc/handlers/Handlers.h
@@ -57,6 +57,7 @@ Json::Value doLedgerHeader          (RPC::JsonContext&);
 Json::Value doLedgerRequest         (RPC::JsonContext&);
 Json::Value doLogLevel              (RPC::JsonContext&);
 Json::Value doLogRotate             (RPC::JsonContext&);
+Json::Value doManifest              (RPC::JsonContext&);
 Json::Value doNoRippleCheck         (RPC::JsonContext&);
 Json::Value doOwnerInfo             (RPC::JsonContext&);
 Json::Value doPathFind              (RPC::JsonContext&);
@@ -86,6 +87,7 @@ Json::Value doValidationCreate      (RPC::JsonContext&);
 Json::Value doWalletPropose         (RPC::JsonContext&);
 Json::Value doValidators            (RPC::JsonContext&);
 Json::Value doValidatorListSites    (RPC::JsonContext&);
+Json::Value doValidatorInfo         (RPC::JsonContext&);
 } // ripple
 
 #endif

src/ripple/rpc/handlers/Manifest.cpp
@@ -1,0 +1,75 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2019 Dev Null Productions
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#include <ripple/app/main/Application.h>
+#include <ripple/basics/base64.h>
+#include <ripple/json/json_value.h>
+#include <ripple/protocol/ErrorCodes.h>
+#include <ripple/protocol/jss.h>
+#include <ripple/rpc/Context.h>
+
+namespace ripple {
+Json::Value doManifest (RPC::JsonContext& context)
+{
+    auto& params = context.params;
+
+    if (!params.isMember(jss::public_key))
+        return RPC::missing_field_error (jss::public_key);
+
+    auto const requested = params[jss::public_key].asString();
+
+    Json::Value ret;
+    ret[jss::requested] = requested;
+
+    auto const pk = parseBase58<PublicKey>(TokenType::NodePublic, requested);
+    if (!pk)
+    {
+        RPC::inject_error(rpcINVALID_PARAMS, ret);
+        return ret;
+    }
+
+    // first attempt to use params as ephemeral key,
+    // if this lookup succeeds master key will be returned,
+    // else pk will just be returned and we will assume that
+    // is master key anyways
+    auto const mk = context.app.validatorManifests().getMasterKey(*pk);
+
+    auto const ek = context.app.validatorManifests().getSigningKey(mk);
+
+    // if ephemeral key not found, we don't have specified manifest
+    if (ek == mk)
+        return ret;
+
+    if (auto const manifest = context.app.validatorManifests().getManifest(mk))
+        ret[jss::manifest] = base64_encode(*manifest);
+    Json::Value details;
+
+    details[jss::master_key] = toBase58(TokenType::NodePublic, mk);
+    details[jss::ephemeral_key] = toBase58(TokenType::NodePublic, ek);
+
+    if (auto const seq = context.app.validatorManifests().getSequence(mk))
+        details[jss::seq] = *seq;
+
+    if (auto const domain = context.app.validatorManifests().getDomain(mk))
+        details[jss::domain] = *domain;
+
+    ret[jss::details] = details;
+    return ret;
+}
+} // ripple

src/ripple/rpc/handlers/ValidatorInfo.cpp
@@ -1,0 +1,62 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of rippled: https://github.com/ripple/rippled
+    Copyright (c) 2019 Dev Null Productions
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose  with  or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+    MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+//==============================================================================
+
+#include <ripple/app/misc/ValidatorKeys.h>
+#include <ripple/app/main/Application.h>
+#include <ripple/basics/base64.h>
+#include <ripple/json/json_value.h>
+#include <ripple/protocol/ErrorCodes.h>
+#include <ripple/protocol/jss.h>
+#include <ripple/rpc/Context.h>
+
+namespace ripple {
+Json::Value doValidatorInfo (RPC::JsonContext& context)
+{
+    // return error if not configured as validator
+    if (context.app.getValidationPublicKey().empty())
+      return RPC::not_validator_error ();
+
+    Json::Value ret;
+
+    auto const pk = context.app.getValidationPublicKey();
+
+    // assume pk is ephemeral key, get master key
+    auto const mk = context.app.validatorManifests().getMasterKey(pk);
+    ret[jss::master_key] = toBase58(TokenType::NodePublic, mk);
+
+    // pk is maskter key, eg no ephemeral key, eg no manifest, just return
+    if (mk == pk)
+        return ret;
+
+    // lookup ephemeral key
+    auto const ek = context.app.validatorManifests().getSigningKey(mk);
+    ret[jss::ephemeral_key] = toBase58(TokenType::NodePublic, ek);
+
+    if (auto const manifest = context.app.validatorManifests().getManifest(mk))
+        ret[jss::manifest] = base64_encode(*manifest);
+
+    if (auto const seq = context.app.validatorManifests().getSequence(mk))
+        ret[jss::seq] = *seq;
+
+    if (auto const domain = context.app.validatorManifests().getDomain(mk))
+        ret[jss::domain] = *domain;
+
+    return ret;
+}
+} // ripple