undertome/rippled

Implement node-to-shard RPC control

src/ripple/rpc/impl/Handler.cpp
@@ -107,10 +107,7 @@ Handler const handlerArray[]{
     {"log_level", byRef(&doLogLevel), Role::ADMIN, NO_CONDITION},
     {"logrotate", byRef(&doLogRotate), Role::ADMIN, NO_CONDITION},
     {"manifest", byRef(&doManifest), Role::USER, NO_CONDITION},
-    {"nodetoshard_status",
-     byRef(&doNodeToShardStatus),
-     Role::USER,
-     NO_CONDITION},
+    {"node_to_shard", byRef(&doNodeToShard), Role::USER, NO_CONDITION},
     {"noripple_check", byRef(&doNoRippleCheck), Role::USER, NO_CONDITION},
     {"owner_info", byRef(&doOwnerInfo), Role::USER, NEEDS_CURRENT_LEDGER},
     {"peers", byRef(&doPeers), Role::ADMIN, NO_CONDITION},

beartec-jpg/FalconLedger

src/ripple/rpc/impl/Handler.cpp
@@ -107,10 +107,7 @@ Handler const handlerArray[]{
     {"log_level", byRef(&doLogLevel), Role::ADMIN, NO_CONDITION},
     {"logrotate", byRef(&doLogRotate), Role::ADMIN, NO_CONDITION},
     {"manifest", byRef(&doManifest), Role::USER, NO_CONDITION},
-    {"nodetoshard_status",
-     byRef(&doNodeToShardStatus),
-     Role::USER,
-     NO_CONDITION},
+    {"node_to_shard", byRef(&doNodeToShard), Role::ADMIN, NO_CONDITION},
     {"noripple_check", byRef(&doNoRippleCheck), Role::USER, NO_CONDITION},
     {"owner_info", byRef(&doOwnerInfo), Role::USER, NEEDS_CURRENT_LEDGER},
     {"peers", byRef(&doPeers), Role::ADMIN, NO_CONDITION},